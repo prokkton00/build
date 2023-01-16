35a93bf90a4c073f152993cf6737b841beca1a68
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-17 04:59:01 +0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -11,28 +11,23 @@ echo "============================"
 # Change to the Output Directory
 cd out/target/product/lmi
 
+# List the Files
+echo "Files inside $HOME/work/out/target/product/lmi are:"
+ls --color=auto
+
 # Set FILENAME var
 FILENAME=evolution*.zip
 
 # Upload to oshi.at
 if [ -z "$TIMEOUT" ];then
-    TIMEOUT=20160 # 14 Days
+    TIMEOUT=20160
 fi
 
-# Upload to WeTransfer
-# NOTE: the current Docker Image, "ghcr.io/sushrut1101/docker:latest", includes the 'transfer' binary by Default
-transfer wet $FILENAME > link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
-
-# Mirror to oshi.at
-curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; }
-
-DL_LINK=$(cat link.txt | grep Download | cut -d\  -f3)
-MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1)
+curl -T $FILENAME https://oshi.at/${FILENAME}/${TIMEOUT} | tee link.txt > /dev/null || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
 
 # Show the Download Link
 echo "=============================================="
-echo "Download Link: ${DL_LINK}" || { echo "ERROR: Failed to Upload the Build!"; }
-echo "Mirror: ${MIRROR_LINK}" || { echo "WARNING: Failed to Mirror the Build!"; }
+cat link.txt || { echo "ERROR: Failed to Upload the Build!" && exit 1; }
 echo "=============================================="
 
 # Exit
