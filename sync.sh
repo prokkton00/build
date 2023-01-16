cf29ba05035a3228f972290bcc90a811151b5699
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-17 04:31:04 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Make the Directory if it doesn't exist
-mkdir -p $SYNC_PATH
+mkdir -p $HOME/work
 
 # Change to the Source Directory
-cd $SYNC_PATH
+cd $HOME/work
 
 # Init Repo
-repo init -u $MANIFEST -b $MANIFEST_BRANCH --depth=1
+repo init -u https://github.com/Evolution-X/manifest.git -b tiramisu --depth=1
 
 # Sync the Sources
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 # Clone Trees
-git clone --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-git clone --depth=1 $VT_LINK $VT_PATH
-git clone --depth=1 $KT_LINK $KT_PATH
+git clone --depth=1 https://github.com/parikk/device_xiaomi_lmi.git -b 13-evox device/xiaomi/lmi || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+git clone --depth=1 https://github.com/parikk/vendor_xiaomi_lmi.git -b 13-evox vendor/xiaomi/lmi
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_lmi.git -b zen_plus-13 kernel/xiaomi/lmi
 
 # Custom
 git clone --depth=1 https://gitlab.com/Roxor-007/WeebX_clang16.git -b main prebuilts/clang/host/linux-x86/clang-weebx
