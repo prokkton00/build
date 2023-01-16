0d9e1eb01eefd494e5f095d25613b889351f6f8c
prokkton00 <98280328+prokkton00@users.noreply.github.com>
2023-01-17 04:34:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,32 +1,19 @@
 #!/bin/bash
 
 # Change to the Source Directory
-cd $SYNC_PATH
+cd $HOME/work
 
 # Set-up ccache
-if [ -z "$CCACHE_SIZE" ]; then
-    ccache -M 10G
-else
-    ccache -M ${CCACHE_SIZE}
-fi
-
-# Run the Extra Command
-$EXTRA_CMD
+cache -M 30G
 
 # Prepare the Build Environment
-source build/envsetup.sh
+. build/envsetup.sh
 
 # lunch the target
-lunch ${LUNCH_COMBO} || { echo "ERROR: Failed to lunch the target!" && exit 1; }
+lunch evolution_lmi-user || { echo "ERROR: Failed to lunch the target!" && exit 1; }
 
 # Build the Code
-if [ -z "$J_VAL" ]; then
-    mka -j$(nproc --all) $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-elif [ "$J_VAL"="0" ]; then
-    mka $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-else
-    mka -j${J_VAL} $TARGET || { echo "ERROR: Build Failed!" && exit 1; }
-fi
+mka evolution
 
 # Exit
 exit 0
